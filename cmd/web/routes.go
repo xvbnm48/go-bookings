72920a612cec1a4b5d135ee089bd020b96df77f1
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-07-08 09:27:13 +0700

    add images statis

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,16 +10,16 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 	mux := chi.NewRouter()
+
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
-	// mux.Use(WriteToConsole)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
 	return mux
 }
